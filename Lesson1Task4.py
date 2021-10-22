b33e4ba3969fe61cc0a75a7d61f2151108b01b2d
Bessarab <39691919+Bessarabia@users.noreply.github.com>
2021-10-22 18:30:06 +0300

    Update Lesson1Task4.py

--- Lesson1Task4.py
+++ Lesson1Task4.py
@@ -3,6 +3,13 @@
 # Для решения используйте цикл while и арифметические операции.
 # get the string, get the length of the string, cycle through the length comparing digit by digit, print the maxDigit
 
-while True
-  break
+userInput = input("Provide the number: ")
+loopCounter = len(userInput) - 1
+userInt = int(userInput)
+
+while loopCounter > 0
+  digit = userint%10^loopcounter
+  nextdigit = userint%10^(loopcounter - 1)
+  maxDigit = max(digit,nextdigit)
+  loopCounter--
   continue
