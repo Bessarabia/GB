012b3d99bf570a965b36c21a030d0a521ceab5df
Bessarab <39691919+Bessarabia@users.noreply.github.com>
2021-10-22 18:04:29 +0300

    Create Lesson1Task4.py

---
+++ Lesson1Task4.py
@@ -0,0 +1,8 @@
+# Пользователь вводит целое положительное число. 
+# Найдите самую большую цифру в числе. 
+# Для решения используйте цикл while и арифметические операции.
+# get the string, get the length of the string, cycle through the length comparing digit by digit, print the maxDigit
+
+while True
+  break
+  continue
