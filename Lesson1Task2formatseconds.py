c5adc136a1266d903ec4e2f4944a5ba2e8e9c9e1
Bessarab <39691919+Bessarabia@users.noreply.github.com>
2021-11-03 14:37:35 +0300

    Delete Lesson1Task2formatseconds.py

--- Lesson1Task2formatseconds.py
+++
@@ -1,5 +0,0 @@
-secondsCount = int(input("Please, input an integer number of seconds to be formatted: "))
-hoursCount = secondsCount//3600
-remainingMinutes = (secondsCount - hoursCount*3600)//60
-remainingSeconds = (secondsCount - hoursCount*3600 - remainingMinutes*60)
-print ("{:02d}".format(hoursCount),":","{:02d}".format(remainingMinutes),":","{:02d}".format(remainingSeconds))
