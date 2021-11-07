d07678a5f58113774a32bc98e00ca5d9523d4c5b
Bessarab <39691919+Bessarabia@users.noreply.github.com>
2021-11-07 22:40:25 +0300

    Update L3T5.py

--- L3T5.py
+++ L3T5.py
@@ -1,4 +1,3 @@
-#5. Программа запрашивает у пользователя строку чисел, разделенных пробелом.
 # При нажатии Enter должна выводиться сумма чисел.
 # Пользователь может продолжить ввод чисел, разделенных пробелом и снова нажать Enter.
 # Сумма вновь введенных чисел будет добавляться к уже подсчитанной сумме.
@@ -6,5 +5,26 @@
 # Если специальный символ введен после нескольких чисел,
 # то вначале нужно добавить сумму этих чисел к полученной ранее сумме и после этого завершить программу.
 
+totalSum = 0
 userString = input("Please input some integers separated by blank space to calculate  sum or 'z' to exit: ")
-conList = []
+conList = userString.split()
+
+lenList = len(conList)
+counter1 = 0
+
+while True:
+    try:
+        s = int(conList[counter1])
+        totalSum += s
+        counter1 += 1
+        print(totalSum)
+    except ValueError:
+        print("incorrect input")
+    break
+    finally:
+        userString = input("Please input some integers separated by blank space to calculate  sum or 'z' to exit: ")
+        conList = userString.split()
+        lenList = len(conList)
+        counter1 = 0
+
+print(totalSum)
