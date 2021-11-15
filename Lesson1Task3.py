a9661605b54cd2361b4a8ade661dd03ad796a94b
Bessarab <39691919+Bessarabia@users.noreply.github.com>
2021-11-15 12:52:47 +0300

    Delete Lesson1Task3.py

--- Lesson1Task3.py
+++
@@ -1,8 +0,0 @@
-#Узнайте у пользователя число n. Найдите сумму чисел n +
-#nn + n Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369
-baseNumber=input("Input base number ")
-n = int(baseNumber)
-nn = int(baseNumber+baseNumber)
-nnn = int(baseNumber+baseNumber+baseNumber)
-execSum = n + nn + nnn
-print(f"{n} + {nn} + {nnn} = {execSum}")
